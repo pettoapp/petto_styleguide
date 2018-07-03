20d7e9a94905c2cf03f2f6f91edc3c5d90c39e4c
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2018-07-03 10:33:24 -0700

    Makes webpack available for other hosts

--- styleguide.config.js
+++ styleguide.config.js
@@ -5,5 +5,11 @@ module.exports = Object.assign(createStyleguideConfig(), {
     serverPort: process.env.PORT ? parseInt(process.env.PORT) : 6060,
     styleguideComponents: {
         Wrapper: path.join(__dirname, 'src/styleguidist/components/ThemeWrapper'),
+    },
+    dangerouslyUpdateWebpackConfig(webpackConfig, env) {
+      webpackConfig.devServer = {
+        disableHostCheck: true
+      }
+      return webpackConfig
     }
 });
