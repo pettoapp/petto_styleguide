0768c2832cfcf28dc34675038a0666c14e685cf3
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2018-07-03 10:19:21 -0700

    Adds fallback for node port

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const createStyleguideConfig = require('create-react-styleguide').createStyleguideConfig;
 
 module.exports = Object.assign(createStyleguideConfig(), {
+    serverPort: process.env.PORT || 6060,
     styleguideComponents: {
         Wrapper: path.join(__dirname, 'src/styleguidist/components/ThemeWrapper'),
     }
